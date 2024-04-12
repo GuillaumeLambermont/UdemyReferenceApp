b2762af43aa3d75dfb73e259e3648d831afddf2e
GuillaumeLambermont <guillaume.lambermont@gmail.com>
2024-04-12 20:09:13 +0200

    Maven and Spring files

---
+++ src/main/java/com/gui/udemyreferenceapp/UdemyReferenceAppApplication.java
@@ -0,0 +1,13 @@
+package com.gui.udemyreferenceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyReferenceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyReferenceAppApplication.class, args);
+	}
+
+}
